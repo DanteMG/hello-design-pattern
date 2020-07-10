bcc7fb4d83b160b8bed073fdad8e1200cb7623a5
王旌舟 <wangjingzhou@meituan.com>
2020-07-10 15:56:07 +0800

    练习github使用

---
+++ src/test/java/NewTest.java
@@ -0,0 +1,15 @@
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+/**
+ * @author wangjingzhou
+ * @create 2020/7/10 3:38 PM
+ * @Description
+ */
+@Slf4j
+public class NewTest {
+    @Test
+    public void simpleTest(){
+        log.info("ddfff");
+    }
+}
